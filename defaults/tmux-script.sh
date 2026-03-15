4f95dd1a52de0741e8957c695345c94ebd44e6e2
Juergen Kajdocsy <juergen.kajdocsy@gmail.com>
2026-03-15 23:58:31 +0100

    changed default tmux-script to only call nix-shell *if* a shell.nix exists

--- defaults/tmux-script.sh
+++ defaults/tmux-script.sh
@@ -16,13 +16,15 @@ tmux resize-pane -y 5 -t "$LEFT_BOTTOM_PANE"
 # --- ADD YOUR COMMANDS HERE ---
 
 # Left top pane: main development pane
-tmux send-keys -t "$LEFT_TOP_PANE" "nix-shell" C-m
-# sleep 10
+if [ -f shell.nix ]; then
+	tmux send-keys -t "$LEFT_TOP_PANE" "nix-shell" C-m
+fi
 tmux send-keys -t "$LEFT_TOP_PANE" "clear && glow README.md" C-m
 
 # Right pane: documentation, logs, or auxiliary tools
-tmux send-keys -t "$RIGHT_PANE" "nix-shell" C-m
-# sleep 10
+if [ -f shell.nix ]; then
+	tmux send-keys -t "$RIGHT_PANE" "nix-shell" C-m
+fi
 tmux send-keys -t "$RIGHT_PANE" "opencode --continue" C-m
 
 # Left bottom pane: local shell for quick commands
